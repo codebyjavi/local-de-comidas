3330454bca68ab36ba473f113485bf2104fb114e
javierspr <learn.javier@gmail.com>
2024-07-01 18:14:55 -0300

    add placeOrder action

--- src/components/OrderTotals.tsx
+++ src/components/OrderTotals.tsx
@@ -1,14 +1,15 @@
 import { useMemo } from "react"
 import { OrderItem } from "../types"
 import { formatCurrency } from "../helpers"
+import { OrderActions } from "../reducers/order-reducer"
 
 type OrderTotalsProps = {
     order: OrderItem[],
     tip: number,
-    placeOrder: () => void
+    dispatch: React.Dispatch<OrderActions>
 }
 
-export const OrderTotals = ({order, tip, placeOrder} : OrderTotalsProps) => {
+export const OrderTotals = ({order, tip, dispatch} : OrderTotalsProps) => {
 
     const subtotalAmount= useMemo(() => order.reduce((total, item) => total + (item.price * item.quantity), 0), [order])
     const tipAmount = useMemo(() => subtotalAmount * tip, [tip, order])
@@ -32,7 +33,7 @@ export const OrderTotals = ({order, tip, placeOrder} : OrderTotalsProps) => {
         </div>
 
          <button className="w-full bg-neutral-950 hover:bg-neutral-700 p-3 uppercase text-white font-bold mt-10"
-         onClick={placeOrder}
+         onClick={() => dispatch({type: "place-order"})}
          >
             Guardar Orden
          </button>
